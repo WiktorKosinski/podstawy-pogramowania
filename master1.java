d085225a896f46f33800a8b13c1ee1ec132af127
Wiktor Kosiński <wiki.kosinski@gmail.com>
2024-03-03 20:39:23 +0100

    zad1

---
+++ master1.java
@@ -0,0 +1,16 @@
+public class master1 {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 3;
+        int c = a - b;
+        int d = a * b;
+        int e = a / b;
+        int sum = a + b;
+        c= a - b;
+
+        System.out.println("Suma: " + sum );
+        System.out.println("Odejmowanie: " + c );
+        System.out.println("Mnożenie: " + d );
+        System.out.println("Dzielenie: " + e );
+    }
+}
