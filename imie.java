e4c565c4457d87546f61ed1e4bcca9fbd4240483
Wiktor Kosiński <wiki.kosinski@gmail.com>
2024-03-04 11:43:31 +0100

    chore: project init

---
+++ imie.java
@@ -0,0 +1,7 @@
+public class imie {
+    public static void main(String[] args) {
+        String fullname = "Wiktor Kosiński";
+     int albumNumber = 51387;
+  System.out.println("Data: " + fullname + " " + albumNumber );
+    }
+}
--- master.java
+++ master.java
@@ -1,10 +1,7 @@
 public class master {
     public static void main(String[] args) {
       System.out.println("Hello World");
-    String fullname = "Wiktor Kosiński";
-  int albumNumber = 51387;
-  //
-  System.out.println("Data: " + fullname + " " + albumNumber );
+  
   }
     
 }
